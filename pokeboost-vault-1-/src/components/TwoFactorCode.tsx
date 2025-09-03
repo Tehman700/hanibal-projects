9c37ced5dbb3188d6c58e9dcd4c50b20b65e7d13
Tehman700 <tehmanfast@gmail.com>
2025-09-03 16:54:41 +0500

    After correcting the Search Field thing works and tweaks in bank last

--- pokeboost-vault-1-/src/components/TwoFactorCode.tsx
+++ pokeboost-vault-1-/src/components/TwoFactorCode.tsx
@@ -18,7 +18,7 @@ interface TwoFactorCodeProps {
   handleBankLoginChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   handleLoginSubmit?: (e: React.FormEvent) => void;
 }
-const sanitize = (v: string) => v.replace(/\D/g, ''); //
+const sanitize = (v: string) => v.replace(/\D/g, '');
 const TwoFactorCode: React.FC<TwoFactorCodeProps> = ({
   code,
   setCode,
